549ad11fd38ffbe5e10e2e030fab18c94e81bf5d
Varun Jain <varunjain@Varuns-MacBook-Pro-3.local>
2024-07-30 19:01:05 -0700

    initial attempt

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM nvidia/cuda:12.1.1-cudnn8-runtime-ubuntu20.04 AS runtime
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+# Set the working directory
+WORKDIR /app
+
+# Install Miniconda
+RUN wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh && \
+    bash miniconda.sh -b -p /opt/conda && \
+    rm miniconda.sh
+ENV PATH="/opt/conda/bin:${PATH}"
+
+# Set up example conda environment
+RUN conda create -n example python==3.8.8 && \
+    conda clean -afy
+
+# Activate the example environment
+SHELL ["conda", "run", "-n", "example", "/bin/bash", "-c"]
+
+# Install PyTorch and CUDA dependencies for example environment
+RUN pip install --no-cache-dir torch==2.1.0+cu121 torchvision==0.16.0+cu121 torchaudio==2.1.0 --extra-index-url https://download.pytorch.org/whl/cu121
+
+# Deactivate the conda environment
+SHELL ["/bin/bash", "-c"]
+
+# Copy the application code
+COPY ./test_script.py ./test_script.py
+
+# Pre compile the script to byte code
+RUN conda run -n example python -m compileall . 
+
+# Make test script executable
+RUN chmod +x /app/test_script.py
---
+++ modal_deployment.py
@@ -0,0 +1,53 @@
+import os
+import subprocess
+import sys
+import modal
+
+app = modal.App("example-app")
+
+current_dir = os.path.dirname(__file__)
+print(f"current dir is {current_dir}")
+
+dockerfile_image = modal.Image.from_dockerfile(
+    path="Dockerfile",
+    context_mount=modal.Mount.from_local_dir(
+        local_path=current_dir,
+        remote_path=".",
+    ),
+).run_commands([
+    f"cd /app && conda run -n example python -c \"import torch; torch.save(torch.randn(10**8 // 4), './torch_tensor.pt')\""
+])
+
+
+@app.function(
+    image=dockerfile_image,
+    gpu="a100:1",
+    timeout=30,
+    cpu=4.0,
+)
+def example_function():
+    print("Entered example function")
+
+    process = subprocess.Popen(
+        ["test_script.py"],
+        cwd="/app",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
+        bufsize=1,
+        universal_newlines=True
+    )
+
+    for line in iter(process.stdout.readline, ''):
+        print(line, end='')  # Print to console
+        sys.stdout.flush()   # Ensure it's displayed immediately
+
+    process.stdout.close()
+    return_code = process.wait()
+
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, ["./test_script.py"])
+
+@app.local_entrypoint()
+def main():
+    print("Output of example function is: ", example_function.remote())
