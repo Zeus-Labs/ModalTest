c009b4256ce5f03bfc6c2bb62aa0d7b7ae40bce7
Varun Jain <varunjain@Varuns-MacBook-Pro-3.local>
2024-07-30 19:54:08 -0700

    tweaks

--- modal_deployment.py
+++ modal_deployment.py
@@ -15,7 +15,7 @@ dockerfile_image = modal.Image.from_dockerfile(
         remote_path=".",
     ),
 ).run_commands([
-    f"cd /app && conda run -n example python -c \"import torch; torch.save(torch.randn(10**8 // 4), './torch_tensor.pt')\""
+    f"cd /app && conda run -n example python -c \"import torch; torch.save(torch.randn(10**9 // 4), './torch_tensor.pt')\""
 ])
 
 
@@ -29,7 +29,7 @@ def example_function():
     print("Entered example function")
 
     process = subprocess.Popen(
-        ["conda", "run", "-n", "example", "python", "test_script.py"],
+        ["conda", "run", "-n", "example", "--no-capture-output", "python", "-u", "test_script.py"],
         cwd="/app",
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
@@ -46,7 +46,10 @@ def example_function():
     return_code = process.wait()
 
     if return_code:
-        raise subprocess.CalledProcessError(return_code, ["conda", "run", "-n", "example", "python", "test_script.py"])
+        raise subprocess.CalledProcessError(
+            return_code,
+            ["conda", "run", "-n", "example", "--no-capture-output", "python", "-u", "test_script.py"]
+        )
 
 @app.local_entrypoint()
 def main():
